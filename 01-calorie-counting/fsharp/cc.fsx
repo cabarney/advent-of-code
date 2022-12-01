3c1055d26a2a6270b8811c728b1fa17dd9788ae4
Barney, Adam <adambarney@rocketmortgage.com>
2022-12-01 14:29:02 -0600

    Day 1

---
+++ 01-calorie-counting/fsharp/cc.fsx
@@ -0,0 +1,14 @@
+open System.IO
+open System.Linq
+
+printfn "Day 1: Calorie Counting (F#)"
+
+let elves = 
+  File.ReadAllText("../input.txt").Split "\n\n"
+  |> Seq.map (fun x -> x.Split("\n"))
+  |> Seq.map (fun x -> x |> Seq.map (fun y -> int y))
+  |> Seq.map (fun x -> x.Sum())
+  |> Seq.sortDescending
+
+printfn "The elf with the most calories has %i" (elves.First())
+printfn "The top 3 elves are carrying a total of %i calories" (elves |> Seq.take 3 |> Seq.sum)
