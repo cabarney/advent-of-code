32956778a0ed9c68e9999bac66e9df70e37eb9f9
Barney, Adam <adambarney@rocketmortgage.com>
2023-01-03 18:42:28 +0000

    Days 24 & 25

--- XX-Template/fsharp/program.fsx
+++
@@ -1,6 +0,0 @@
-open System.IO
-
-let inputPath = Path.Combine(__SOURCE_DIRECTORY__, "../input.txt")
-let input = File.ReadAllLines(inputPath) |> Array.map (fun x -> x.Trim())
-
-printfn "Day X:  (fsharp)"
--- XX-Template/python/program.py
+++
@@ -1,7 +0,0 @@
-from os import path
-
-basepath = path.dirname(__file__)
-inputPath = path.abspath(path.join(basepath, "..", "input.txt"))
-input = [line.strip() for line in open(inputPath, 'r').readlines()]
-
-print("Day X:  (python)")
